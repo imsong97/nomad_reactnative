f2c6e1b283e73a482057ee4ae2292fcebd818d0e
DyNamiC <imsong1997@gmail.com>
2020-04-01 22:01:47 +0900

    #1 logic

--- weatherapp/App.js
+++ weatherapp/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+ import { Alert } from "react-native";
+ import Loading from "./Loading";
+ import * as Location from "expo-location";
+ import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello App</Text>
-    </View>
-  );
-}
+ const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ export default class extends React.Component {
+   state = {
+     isLoading: true
+   };
+
+   getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+
+   getLocation = async () => {
+     try {
+       await Location.requestPermissionsAsync();
+       const {
+         coords: { latitude, longitude }
+       } = await Location.getCurrentPositionAsync();
+       this.getWeather(latitude, longitude);
+       this.setState({ isLoading: false });
+     } catch (error) {
+       Alert.alert("Can't find you.", "So sad");
+     }
+   };
+
+   componentDidMount() {
+     this.getLocation();
+   }
+   render() {
+     const { isLoading } = this.state;
+     return isLoading ? <Loading /> : null;
+   }
+ }
